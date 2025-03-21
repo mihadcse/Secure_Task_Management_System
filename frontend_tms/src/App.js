abfc7ffddd7e32594f427c5dd19a09322dc89f03
mihadcse <huzzatullah@iut-dhaka.edu>
2025-03-21 23:42:25 +0600

    Install tailwind css and make Navbar component. Also use react router dom for differents routes in frontend

--- frontend_tms/src/App.js
+++ frontend_tms/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router >
   )
 }
 
---
+++ frontend_tms/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <nav className="bg-slate-900 p-4 shadow-md">
+            <ul className="flex justify-center space-x-6 text-white font-semibold">
+                <li>
+                    <Link to="/" className="hover:text-gray-200">Secure Task Management System(TMS)</Link>
+                </li>
+                <li>
+                    <Link to="/tasks" className="hover:text-gray-200">Tasks</Link>
+                </li>
+                <button className='bg-indigo-50 text-indigo-950 w-20 h-7 rounded-md'>
+                    <Link to="/login" className="hover:text-cyan-500">Login</Link>
+                </button>
+                <button className='bg-indigo-50 text-indigo-950 w-20 h-7.5 rounded-md'>
+                    <Link to="/register" className="hover:text-cyan-500">Register</Link>
+                </button>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
