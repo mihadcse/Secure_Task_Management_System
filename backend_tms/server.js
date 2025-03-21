52233af2f6b071ce6c45ad8b9e3756846bad1632
mihadcse <huzzatullah@iut-dhaka.edu>
2025-03-21 21:43:41 +0600

    Connected to mongodb

--- backend_tms/server.js
+++ backend_tms/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
@@ -8,6 +9,13 @@ const PORT = 5000;
 dotenv.config();
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
+;
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
